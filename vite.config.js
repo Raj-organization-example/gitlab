bce628e81644889439212bac94b47312df51eedb
Rajcommit <raj.vbeyond@gmail.com>
2025-07-08 11:51:17 +0530

    fix: set Vite base path for Github Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/gitlab/', // 👈 this tells Vite to generate links correctly for GitHub Pages
   test: {
     globals: true,
     environment: 'jsdom',
